733e62b41384cbaa907efe980af95a57662b656f
Gökçe Baykal <114662360+gokcebaykaal@users.noreply.github.com>
2025-04-08 18:57:11 +0300

    new

---
+++ src/components/GetStarted/GetStarted.jsx
@@ -0,0 +1,23 @@
+import React from 'react'
+import "./GetStarted.css"
+const GetStarted = () => {
+  return (
+    <div>
+      <section className="g-wrapper">
+        <div className="paddings innerWidth g-container">
+            <div className="flexColCenter inner-container">
+                <span className='primaryText'>Get started with Homyz</span>
+                <span className='secondaryText'>Subscribe and 
+                find super attractive price quotes from us.
+                Find your residence soon.</span>
+                <button className='button'>
+                    <a href='gokce25baykal0702@gmail.com'>Get Started</a>
+                </button>
+            </div>
+        </div>
+      </section>
+    </div>
+  )
+}
+
+export default GetStarted
